1dd0b6559560fca0d964c0adfd1b2a63750bb009
True Server Admin <admin@trueserver.com>
2026-09-23 23:37:30 +0300

    Upload game

--- server/index.js
+++ server/index.js
@@ -1,9 +1,4 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
@@ -12,7 +7,6 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 const app = express();
 app.use(cors());
