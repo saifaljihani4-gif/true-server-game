5977e6cfadb30028876b4448c9c4c4455fa50260
True Server Admin <admin@trueserver.com>
2026-09-23 23:25:42 +0300

    Upload game

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
@@ -288,5 +293,10 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3001;
+app.use(express.static(path.join(__dirname, '../dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => console.log(`Socket server running on port ${PORT}`));
